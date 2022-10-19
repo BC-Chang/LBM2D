1190cd3b913a4a68eaa9bb55c4bccbe0ddb5159b
BC-Chang <bcchang@utexas.edu>
2022-10-19 15:09:40 -0500

    streaming and plotting

--- LBM_single_phase_beadspack.py
+++ LBM_single_phase_beadspack.py
@@ -16,17 +16,18 @@ from time import perf_counter_ns
 # Initialization
 tau = 1.0  # Relaxation time
 g = 0.00001  # Gravity or other force
-density = 1
-tf = 10  # Maximum number of iteration steps
+density = 1.
+tf = 2  # Maximum number of iteration steps
 precision = 1.E-5  # Convergence criterion
 vold = 1000
 
-check_convergence = 100  # Check convergence every [check_convergence] time steps
+check_convergence = 1  # Check convergence every [check_convergence] time steps
 
 
 # Read in and create geometry
 data = tifffile.imread("beads_dry.slice150.sub.tif")  # Data labels are 0 and 255
 data = data / 255  # Make Data 0 and 1s
+data = data.astype(np.uint8)
 
 # Solid nodes are labeled 1, fluid nodes are labeled 0
 is_solid_node = data
@@ -42,9 +43,9 @@ f = np.array([4./9.,
               1./36.,
               1./36.,
               1./36.,
-              1./36.]) * density
+              1./36.], dtype=np.single) * density
 # Broadcast to 3D array with each slice corresponding to
-f = np.tile(f[:, np.newaxis, np.newaxis], (9, nx, ny))
+f = np.tile(f[:, np.newaxis, np.newaxis], (nx, ny)).astype(np.single)
 # Allocate memory to equilibrium functions
 feq = np.empty_like(f)
 ftmp = feq.copy()
@@ -96,7 +97,7 @@ for ts in range(tf):
                 rt2 = 1./36. * rho[j, i]
                 # Add forcing to velocity
                 ueqxij = u_x[j, i] + tau * g
-                ueqyij = u_y[j, i] + tau * g
+                ueqyij = u_y[j, i]
                 uxsq = ueqxij ** 2.
                 uysq = ueqyij ** 2.
                 uxuy5 = ueqxij + ueqyij
@@ -105,14 +106,14 @@ for ts in range(tf):
                 uxuy8 = ueqxij - ueqyij
                 usq = uxsq + uysq
 
-                feq[0, j, i] = rt0 * (1 - f3*usq)
-                feq[1, j, i] = rt1 * (1 + f1 * ueqxij + f2 * uxsq - f3 * usq)
-                feq[2, j, i] = rt1 * (1 + f1 * ueqyij + f2 * uysq - f3 * usq)
-                feq[3, j, i] = rt1 * (1 - f1 * ueqxij + f2 * uxsq - f3 * usq)
-                feq[4, j, i] = rt1 * (1 - f1 * ueqyij + f2 * uysq - f3 * usq)
-                feq[5, j, i] = rt2 * (1 + f1 * uxuy5 + f2 * uxuy5**2 - f3 * usq)
-                feq[6, j, i] = rt2 * (1 + f1 * uxuy6 + f2 * uxuy6**2 - f3 * usq)
-                feq[7, j, i] = rt2 * (1 + f1 * uxuy7 + f2 * uxuy7**2 - f3 * usq)
+                feq[0, j, i] = rt0 * (1                                - f3 * usq)
+                feq[1, j, i] = rt1 * (1 + f1 * ueqxij + f2 * uxsq      - f3 * usq)
+                feq[2, j, i] = rt1 * (1 + f1 * ueqyij + f2 * uysq      - f3 * usq)
+                feq[3, j, i] = rt1 * (1 - f1 * ueqxij + f2 * uxsq      - f3 * usq)
+                feq[4, j, i] = rt1 * (1 - f1 * ueqyij + f2 * uysq      - f3 * usq)
+                feq[5, j, i] = rt2 * (1 + f1 * uxuy5 + f2 * uxuy5 ** 2 - f3 * usq)
+                feq[6, j, i] = rt2 * (1 + f1 * uxuy6 + f2 * uxuy6 ** 2 - f3 * usq)
+                feq[7, j, i] = rt2 * (1 + f1 * uxuy7 + f2 * uxuy7 ** 2 - f3 * usq)
                 feq[8, j, i] = rt2 * (1 + f1 * uxuy8 + f2 * uxuy8 ** 2 - f3 * usq)
 
     # Collision Step
@@ -132,13 +133,13 @@ for ts in range(tf):
                 f[4, j, i] = tmp
 
                 # Node 5 & 7
-                tmp = f[5, j, i];
-                f[5, j, i] = f[7, j, i];
+                tmp = f[5, j, i]
+                f[5, j, i] = f[7, j, i]
                 f[7, j, i] = tmp
 
                 # Node 6 & 8
-                tmp = f[6, j, i];
-                f[6, j, i] = f[8, j, i];
+                tmp = f[6, j, i]
+                f[6, j, i] = f[8, j, i]
                 f[8, j, i] = tmp
 
             else:
@@ -146,12 +147,74 @@ for ts in range(tf):
                 for a in range(9):
                     f[a, j, i] = f[a, j, i] - (f[a, j, i] - feq[a, j, i]) / tau
 
+    # Streaming Step
+    for j in range(ny):
+        if j > 0:
+            j_n = j - 1
+        else:
+            j_n = ny - 1
+
+        if j < ny - 1:
+            j_p = j + 1
+        else:
+            j_p = 0
+
+        for i in range(nx):
+            if i > 0:
+                i_n = i - 1
+            else:
+                i_n = nx - 1
+
+            if i < nx - 1:
+                i_p = i + 1
+            else:
+                i_p = 0
+
+            ftmp[0, j, i] = f[0, j, i]
+            ftmp[1, j, i_p] = f[1, j, i]
+            ftmp[2, j_p, i] = f[2, j, i]
+            ftmp[3, j, i_n] = f[3, j, i]
+            ftmp[4, j_n, i] = f[4, j, i]
+            ftmp[5, j_p, i_p] = f[5, j, i]
+            ftmp[6, j_p, i_n] = f[6, j, i]
+            ftmp[7, j_n, i_n] = f[7, j, i]
+            ftmp[8, j_n, i_p] = f[8, j, i]
+
+    f = ftmp
+
+    # Plot the time step and check convergence every check_convergence time step
+    if ts % check_convergence == 0:
+        u = np.sqrt(u_x**2 + u_y**2)
+        vnew = np.mean(u)
+        error = np.abs(vold - vnew) / vold
+        vold = vnew
+
+        plt.figure(dpi=300)
+        plt.imshow(u, cmap='jet')
+        plt.tight_layout()
+        plt.colorbar()
+        plt.title('Speed Profile')
+        plt.show()
+
+        if error < precision:
+            print('Simulation has converged')
+            break
+
+    if ts == tf:
+        print('Reached maximum iterations')
+
+
+
+
+
+
+
+
 
 
 
 toc = perf_counter_ns()
 print(f"Elapsed Time: {(toc - tic)*1E-9}s")
-
 
 # f = np.apply_along_axis(f *slicewise_initial_distribution, axis=0)
 
